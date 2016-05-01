0ca819fd1e13ed74ff14b43fe5c7ee25d289e27f
anapmc <anapmc.carvalho@gmail.com>
2016-05-01 14:28:32 +0100

    standalone

--- findaplumber.py
+++ findaplumber.py
@@ -1,5 +1,7 @@
 import argparse
 
+
+argparse.PARSER
 # Create a script to scrape the plumbers info, with option to save to db or dawnload to csv
 # Add timestamp to mongo db lib and one more useful field from checkatrade
 # Write tests
